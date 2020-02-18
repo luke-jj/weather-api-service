13d20bf119e586f95ffa8c165ec3e619d35790f6
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-02-18 20:47:45 +0100

    Add stricter validation for city query string param.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.get('/api/v1/weather', async (req, res) => {
     return res.status(400).send('Query string parameter `city` is required.');
   }
 
+  if (!/^[0-9A-Za-z ,]{1,42}$/.test(req.query['city'])) {
+    return res.status(400).send('Illegal city name.');
+  }
+
   try {
     const { data: weather } = await getWeather(req.query['city']);
     return res.send(weather);
